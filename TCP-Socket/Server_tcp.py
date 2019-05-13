8c6371f7296ecc324e48e2d8661cdb0f5337bd4d
Meet Ranoliya <meetranoliya094@gmail.com>
2019-05-13 19:47:24 +0530

    Update Server_tcp.py

--- TCP-Socket/Server_tcp.py
+++ TCP-Socket/Server_tcp.py
@@ -9,7 +9,7 @@ s.bind(("192.168.1.122", port))
 s.listen(1)
 while True:
     c, addr = s.accept()
-    print("Got Connection from = " + addr)
+    print("Got Connection from = " + str(addr))
 
     while True:
         sen = input("Enter Message to Send = ")
@@ -18,7 +18,7 @@ while True:
             break
         msg = c.recv(1024)
         print("Received = " + msg.decode('utf-8'))
-        if msg == 'bye':
+        if msg.decode('utf-8') == 'bye':
             break
     c.close()
     des = input("Do you want to continue? (y or n) : ")
